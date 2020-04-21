49596a53fa0f1fdf136543bd3c292149e58e5bdc
howtowin8 <huangjinchao8@126.com>
2020-04-21 10:50:11 +0800

    错误拦截

--- src/main.js
+++ src/main.js
@@ -1,10 +1,35 @@
 import Vue from 'vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 
+// 根据前端的跨越方式做调整
+axios.defaults.baseURL = '/api';
+
+// 请求超时设置
+axios.defaults.timeout = 8000;
+
+// 定义接口错误拦截器
+
+axios.interceptors.response.use(function(response){
+  let res = response.data;
+  if (res.status == 0){
+    return res.data;
+  }else if(res.data == 10){
+    window.location.href = '/#/login';
+  }else{
+    alert(res.msg);
+  }
+})
+
+Vue.use(VueAxios,axios);
 Vue.config.productionTip = false
 
 new Vue({
   router,
   render: h => h(App),
 }).$mount('#app')
+
+
+// 哈哈哈哈
